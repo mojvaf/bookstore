0f370da9d71a81e732d3f2b453555130362743b1
mojvaf <mojgan@zula-tribe.com>
2025-06-16 14:11:54 +0200

    Error: error in form and model

--- xproject/urls.py
+++ xproject/urls.py
@@ -1,12 +1,13 @@
 
 from django.contrib import admin
 from django.urls import path
-from books.views import BooksPage
-from books.views import BookDetailview
+from books.views import BooksPage, BookDetailview , create_book_form_view
+
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('books/',BooksPage.as_view(), name = 'books_list'),
     path("books/<int:pk>/", BookDetailview.as_view(), name="book_detail"),
+    path('create-book/', create_book_form_view, name='create_new_book'),
 ]
