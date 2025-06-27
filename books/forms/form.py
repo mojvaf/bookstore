51008a12c46c2c81cdae2ebe89293c66b93467e6
mojvaf <mojgan@zula-tribe.com>
2025-06-27 14:06:45 +0200

    feat: HTML

--- books/forms/form.py
+++ books/forms/form.py
@@ -1,10 +1,9 @@
 from django import forms
-from ..models.book import Book
 from ..models.book import Author
 from ..models.book import Publisher
 
 class CreateBookForm(forms.Form):
-      name = forms.CharField(max_length=255)
+      name = forms.CharField(max_length=20)
 
       publisher = forms.ModelChoiceField(
          queryset= Publisher.objects.all(),
