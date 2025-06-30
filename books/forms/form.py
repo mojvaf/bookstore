ab33a61d37c99f555e07128c3493b5b658efd80e
mojvaf <mojgan@zula-tribe.com>
2025-07-01 00:32:49 +0200

    bugs

--- books/forms/form.py
+++ books/forms/form.py
@@ -1,6 +1,7 @@
 from django import forms
 from ..models.book import Author
 from ..models.book import Publisher
+from ..models.book import Book
 
 class CreateBookForm(forms.Form):
       name = forms.CharField(max_length=20)
@@ -18,3 +19,17 @@ class CreateBookForm(forms.Form):
       )
 
       review = forms.CharField(widget=forms.Textarea, required=False) 
+
+
+class BookForm(forms.ModelForm):
+      class Meta:
+            model = Book
+            fields = ['title', 'publication_date', 'author', 'publisher']
+
+            
+
+class ContactForm(forms.Form):
+      name = forms.CharField(max_length=100)
+      email = forms.EmailField()
+      message = forms.CharField(widget=forms.Textarea)
+      
--- xproject/urls.py
+++ xproject/urls.py
@@ -1,7 +1,7 @@
 
 from django.contrib import admin
 from django.urls import path
-from books.views import BooksPage, BookDetailview , create_book_form_view
+from books.views import BooksPage, BookDetailview , create_book_form_view, book_form_view, contact_form_view
 
 
 
@@ -10,4 +10,6 @@ urlpatterns = [
     path('books/',BooksPage.as_view(), name = 'books_list'),
     path("books/<int:pk>/", BookDetailview.as_view(), name="book_detail"),
     path('create-book/', create_book_form_view, name='create_book_form_view'),
+    path('add-book/', book_form_view, name='book_form'),
+    path('contact/', contact_form_view, name='contact_form'),
 ]
